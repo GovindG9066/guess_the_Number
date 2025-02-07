8b22dc79e0701f18524f00aaa222b48ef30816c9
Govind9066 <gadekargovind575@gmail.com>
2025-02-07 12:18:44 +0530

    game commit again

--- Java_Games/src/Games/Guess_The_Number.java
+++ Java_Games/src/Games/Guess_The_Number.java
@@ -3,68 +3,62 @@ package Games;
 import java.util.Random;
 import java.util.Scanner;
 
-	class guess{
-		public int number;
-		public int inputNumber;
-		public int gusses=0;
+class guess{
+	public int number;
+	public int inputNumber;
+	public int gusses=0;
+	
+	public int getGusses() {
+		return gusses;
+	}
+	
+	public void setGusses(int gusses) {
+		this.gusses = gusses;
+	}
+	
+	 guess(){
+		Random rand=new Random();
+		this.number=rand.nextInt(100)+1;		
+	}
+	
+	void userInput() {
+		System.out.println("Guess the Number...");
+		Scanner sc =new Scanner(System.in);
+		this.inputNumber=sc.nextInt();
+		this.gusses++;
 		
-		public int getGusses() {
-			return gusses;
-		}
-		
-		public void setGusses(int gusses) {
-			this.gusses = gusses;
-		}
-		
-		 guess(){
-			Random rand=new Random();
-			this.number=rand.nextInt(100)+1;		
-		}
-		
-		void userInput() {
-			System.out.println("Guess the Number...");
-			Scanner sc =new Scanner(System.in);
-			this.inputNumber=sc.nextInt();
-			this.gusses++;
-			
-			
-		}
-		boolean isCorrect() {
-			if(inputNumber==number) {
-				System.out.println("congrats you guess the number in "+gusses+" attepmt");
-			return true;
-			}
-			else if(inputNumber<number) {
-				System.out.println("Too small...");
-			}
-			else if(inputNumber>number) {
-				System.out.println("Too Large...");
-			}
-			else {
-				return false;			
-			}
-			return false;			
-		}
 		
 	}
-
-
-	public class Guess_The_Number {
-		public static void main(String[] args) {
-				guess g=new guess();
-				boolean b=false;
-				while(!b) {
-				g.userInput();
-				b=g.isCorrect();
-//				g.isCorrect();
-				}
-				
-				
-				
-				
-//				System.out.println(b);
+	boolean isCorrect() {
+		if(inputNumber==number) {
+			System.out.println("congrats you guess the number in "+gusses+" attepmt");
+		return true;
 		}
-
+		else if(inputNumber<number) {
+			System.out.println("Too small...");
+		}
+		else if(inputNumber>number) {
+			System.out.println("Too Large...");
+		}
+		else {
+			return false;			
+		}
+		return false;			
+	}
+	
+}
+	
+public class Guess_The_Number {
+	public static void main(String[] args) {
+			guess g=new guess();
+			boolean b=false;
+			
+			while(!b) {
+			g.userInput();
+			b=g.isCorrect();
+			
+			}
+		}
 	}
 
 
